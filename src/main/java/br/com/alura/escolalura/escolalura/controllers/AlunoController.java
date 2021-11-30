9d19d71736dff0434e33d9ca934da8a29a637dad
Henrique Moreira <henriquemoreiraa@gmail.com>
2021-11-30 12:00:11 -0300

    Corrigindo erro de data e criação da funcionalidade de enviar aluno para o java

--- src/main/java/br/com/alura/escolalura/escolalura/controllers/AlunoController.java
+++ src/main/java/br/com/alura/escolalura/escolalura/controllers/AlunoController.java
@@ -4,13 +4,23 @@ import br.com.alura.escolalura.escolalura.models.Aluno;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AlunoController {
 
     @GetMapping("/aluno/cadastrar")
-    public String cadastrar(Model model) {
+    public String cadastrar(Model model){
+
         model.addAttribute("aluno", new Aluno());
+
         return "aluno/cadastrar";
+    }
+
+    @PostMapping("/aluno/salvar")
+    public String salvar(@ModelAttribute Aluno aluno){
+        System.out.println(aluno);
+        return "redirect:/";
     }
 }
