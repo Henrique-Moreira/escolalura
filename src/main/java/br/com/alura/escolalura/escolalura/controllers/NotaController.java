5e1f03142d5364afbbab8c6b946f202a45a475e0
Henrique Moreira <henriquemoreiraa@gmail.com>
2021-12-02 12:01:38 -0300

    cria pesquisa por nota aprovados ou reprovados

--- src/main/java/br/com/alura/escolalura/escolalura/controllers/NotaController.java
+++ src/main/java/br/com/alura/escolalura/escolalura/controllers/NotaController.java
@@ -6,10 +6,9 @@ import br.com.alura.escolalura.escolalura.repositories.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class NotaController {
@@ -34,5 +33,22 @@ public class NotaController {
         repository.salvar(aluno.adicionar(aluno, nota));
 
         return "redirect:/aluno/listar";
+    }
+
+    @GetMapping("/nota/iniciarpesquisa")
+    public String iniciarPesquisa() {
+        return "nota/pesquisar";
+    }
+
+    @GetMapping("nota/pesquisar")
+    public String pesquisarPor(@RequestParam("classificacao") String classificacao,
+                               @RequestParam("notacorte") String notaCorte,
+                               Model model) {
+
+        List<Aluno> alunos = repository.pesquisarPor(classificacao, Double.parseDouble(notaCorte));
+
+        model.addAttribute("alunos", alunos);
+
+        return "nota/pesquisar";
     }
 }
--- src/main/java/br/com/alura/escolalura/escolalura/repositories/AlunoRepository.java
+++ src/main/java/br/com/alura/escolalura/escolalura/repositories/AlunoRepository.java
@@ -81,6 +81,23 @@ public class AlunoRepository {
         return alunos;
     }
 
+    public List<Aluno> pesquisarPor(String classificacao, double nota) {
+        criarConexao();
+        MongoCollection<Aluno> alunoCollection = this.bancoDeDados.getCollection("alunos", Aluno.class);
+
+        MongoCursor<Aluno> resultados = null;
+        if(classificacao.equals("reprovados")) {
+            resultados = alunoCollection.find(Filters.lt("notas", nota)).iterator();
+        } else if(classificacao.equals("aprovados")){
+            resultados = alunoCollection.find(Filters.gte("notas", nota)).iterator();
+        }
+
+        List<Aluno> alunos = popularAlunos(resultados);
+
+        fecharConexao();
+        return alunos;
+    }
+
     private void fecharConexao() {
         this.cliente.close();
     }
